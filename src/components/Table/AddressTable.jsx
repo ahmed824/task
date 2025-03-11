d66d9f15664f7b9111fc9e2adf8a73338dd99b5d
hasnaa104 <hasnaawarda444@gmail.com>
2025-03-12 00:27:41 +0200

    handle form close

--- src/components/Table/AddressTable.jsx
+++ src/components/Table/AddressTable.jsx
@@ -20,25 +20,25 @@ export default function AddressTable({
   };
 
   return (
-    <div className="mt-6 border bg-white border-gray-200 shadow-sm">
+    <div className="mt-6  bg-transparent rounded-lg">
       {addresses.length > 0 ? (
         addresses.map((address, index) => (
           <React.Fragment key={index}>
             <TableRowItem address={address} index={index} onEdit={handleEdit} onDelete={onDelete} />
             {editingIndex === index && (
-              <div className="mt-2 bg-gray-50 p-4 rounded-md">
+              <div className="mt-2 rounded-md">
                 <AddressForm onClose={() => setEditingIndex(null)} onSave={onSave} initialValues={initialValues} mode="edit" />
               </div>
             )}
           </React.Fragment>
         ))
       ) : (
-        <div className="p-3 text-center text-gray-500">لا توجد عناوين مضافة بعد</div>
+        <div className="p-3 text-center border-1 border-[#ddd] rounded-md text-gray-500">لا توجد عناوين مضافة بعد</div>
       )}
 
       {showForm && editingIndex === null && (
-        <div className="mt-4">
-          <AddressForm onClose={() => setEditingIndex(null)} onSave={onSave} initialValues={initialValues} mode="add" />
+        <div className="mt-2 bg-transparent ">
+          <AddressForm onClose={onClose} onSave={onSave} initialValues={initialValues} mode="add" />
         </div>
       )}
     </div>
