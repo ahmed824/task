748453abed65f6a29d8e46f7da3a4e49fbfe84fb
hasnaa104 <hasnaawarda444@gmail.com>
2025-03-12 00:08:54 +0200

    secoend tab

--- src/components/Table/AddressTable.jsx
+++ src/components/Table/AddressTable.jsx
@@ -1,5 +1,5 @@
 "use client";
-import React from "react";
+import React, { useState } from "react";
 import dynamic from "next/dynamic";
 
 const TableRowItem = dynamic(() => import("./TableRowItem"), { ssr: false });
@@ -7,34 +7,38 @@ const AddressForm = dynamic(() => import("../AddressForm/AddressForm"), { ssr: f
 
 export default function AddressTable({
   addresses,
-  onEdit,
   onDelete,
   showForm,
-  editingIndex,
   initialValues,
   onSave,
   onClose,
 }) {
+  const [editingIndex, setEditingIndex] = useState(null);
+
+  const handleEdit = (index) => {
+    setEditingIndex((prevIndex) => (prevIndex === index ? null : index));
+  };
+
   return (
-    <div className="mt-6 border border-gray-200 rounded-lg shadow-md">
+    <div className="mt-6 border bg-white border-gray-200 shadow-sm">
       {addresses.length > 0 ? (
         addresses.map((address, index) => (
           <React.Fragment key={index}>
-            <TableRowItem address={address} index={index} onEdit={onEdit} onDelete={onDelete} />
-            {showForm && editingIndex === index && (
+            <TableRowItem address={address} index={index} onEdit={handleEdit} onDelete={onDelete} />
+            {editingIndex === index && (
               <div className="mt-2 bg-gray-50 p-4 rounded-md">
-                <AddressForm onClose={onClose} onSave={onSave} initialValues={initialValues} mode="edit" />
+                <AddressForm onClose={() => setEditingIndex(null)} onSave={onSave} initialValues={initialValues} mode="edit" />
               </div>
             )}
           </React.Fragment>
         ))
       ) : (
-        <div className="p-8 text-center text-gray-500">لا توجد عناوين مضافة بعد</div>
+        <div className="p-3 text-center text-gray-500">لا توجد عناوين مضافة بعد</div>
       )}
 
       {showForm && editingIndex === null && (
         <div className="mt-4">
-          <AddressForm onClose={onClose} onSave={onSave} initialValues={initialValues} mode="add" />
+          <AddressForm onClose={() => setEditingIndex(null)} onSave={onSave} initialValues={initialValues} mode="add" />
         </div>
       )}
     </div>
